10b3871300b8c1aff90120f24c2069128761e421
Ian Blanc <ianblancmiranda@gmail.com>
2025-04-02 20:09:48 -0400

    default allowed host

--- app/POWER_shuffle/settings.py
+++ app/POWER_shuffle/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', "setmeinprod")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get("DJANGO_DEBUG", 0)))
 
-ALLOWED_HOSTS = [] if DEBUG else os.environ.get("DJANGO_ALLOWED_HOSTS").split(',')
+DJANGO_ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost")
+ALLOWED_HOSTS = [] if DEBUG else DJANGO_ALLOWED_HOSTS.split(',')
 
 # Application definition
 
